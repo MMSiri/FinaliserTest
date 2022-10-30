c89b0723066056420df6574a317d16017665bc30
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-10-30 18:07:02 +0000

    Add project files.

---
+++ FinaliserTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinaliserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var clones = new List<EvilClone>();
+            while (true)
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'a':
+                        clones.Add(new EvilClone());
+                        break;
+                    case 'c':
+                        Console.WriteLine("Clearing list at time {0}", stopwatch.ElapsedMilliseconds);
+                        clones.Clear();
+                        break;
+                    case 'g':
+                        Console.WriteLine("Collecting at time {0}", stopwatch.ElapsedMilliseconds);
+                        GC.Collect();
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
